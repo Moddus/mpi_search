e82b984ede740898b6c4b8899c81763a002f0017
Markus Boese <markus.boese@gmail.com>
2015-01-06 22:22:19 +0100

    remove the_silver_searcher heeader.

--- src/file_searcher.c
+++ src/file_searcher.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <search.h>
-#include <util.h>
 
 #include "file_searcher.h"
 #include "ps.h"
@@ -10,10 +9,10 @@ ps_file_searcher_create( ps_file_seacher_t** searcher,
                         unsigned int seek_to,
                         FILE* file )
 {
-    *searcher = ag_malloc(sizeof(ps_file_seacher_t));
-    (*searcher)->seek_to = seek_to;
-    (*searcher)->found = -1;
-    (*searcher)->file = file;
+    //*searcher = ag_malloc(sizeof(ps_file_seacher_t));
+    //(*searcher)->seek_to = seek_to;
+    //(*searcher)->found = -1;
+    //(*searcher)->file = file;
 }
 
 void
