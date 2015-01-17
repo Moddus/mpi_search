04fc544f4db7ba727cca2a31f099b2451b4037cc
Markus Boese <markus.boese@gmail.com>
2015-01-17 10:04:08 +0100

    moved mpi depend macro to ps_mpi.h.

--- src/ps_mpi.h
+++ src/ps_mpi.h
@@ -3,6 +3,8 @@
 
 #include <mpi.h>
 
+#define PS_MPI_CHECK_ERR(R) PS_COMP_VAL_AND_GOTO_WITH_RV(R, !=, MPI_SUCCESS, error, PS_MPI_COMMON_ERROR)
+
 #define PS_MPI_CHECK_GOTO_ERROR(rv) PS_CHECK_ERRV_AND_GOTO(rv, MPI_SUCCESS, error)
 
 #endif
