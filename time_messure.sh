138873c52810dfe70ce0e81d6fc863ee56b4b8aa
Mark Bonnekessel <bonnekessel@mailbox.org>
2015-02-02 14:36:19 +0100

    Created a stupid bash-script for performance measurement. Expect a file called testfile in project-root

---
+++ time_messure.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+MAX_N=4;
+CHUNKSTART=1;
+CHUNKSTEP=10;
+CHUNKEND=100;
+
+if [[ $# -eq 1 ]]; then
+    MAX_N=$1;
+fi
+
+if [[ $# -eq 4 ]]; then
+    CHUNKSTART=$2;
+    CHUNKSTEP=$3;
+    CHUNKEND=$4;
+fi
+
+for n in `seq 1 1 $MAX_N`; do
+    innerN=$n
+    for n in `seq $CHUNKSTART $CHUNKSTEP $CHUNKEND`; do
+        START_TIME=$SECONDS
+        chunksize=$((1024*1024*$n))
+        mpirun -n $innerN bin/mpi_searcher -s 23.75.241.41 -f ./testfile -c $chunksize
+        ELAPSED_TIME=$(($SECONDS - $START_TIME))
+        echo process count: $innerN  --  chunkzsize: $chunksize byte  --  time : $ELAPSED_TIME s
+    done
+done
+
